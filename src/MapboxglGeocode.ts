16edcb59452682d05ef673c132f5d1ddc23d900c
wujing <jing.wu@seeed.cc>
2023-06-28 17:41:07 +0800

    refactor: 删掉qs

--- src/MapboxglGeocode.ts
+++ src/MapboxglGeocode.ts
@@ -1,4 +1,3 @@
-import qs from 'qs'
 import { ForwardGeocoding, GeocodeType, ReverseGeocoding } from './types/MapboxglGeocodeType'
 import { checkAccessToken, getAccessToken } from './common'
 import axios from 'axios'
@@ -10,7 +9,7 @@ class MapboxglGeocode {
   private static basicUrl = 'https://api.mapbox.com/geocoding'
   private static currentVersion = 'v5'
 
-  private constructor() {}
+  private constructor() { }
 
   static async reverse(reverseOption: ReverseGeocoding) {
     checkAccessToken()
@@ -18,37 +17,28 @@ class MapboxglGeocode {
       endpoint = 'mapbox.places',
       longitude,
       latitude,
-      reverseMode,
-      limit,
-      routing,
-      ...otherParams
     } = reverseOption
-    const params: Record<string, string> = {
-      access_token: getAccessToken() || '',
-    }
-    if (reverseMode) {
-      params.reverseMode = reverseMode
-    }
-    if (limit) {
-      params.limit = limit.toString()
-    }
-    if (routing) {
-      params.routing = routing.toString()
-    }
-    const query = qs.stringify({
-      ...params,
-      ...otherParams,
-    })
     return (await axios.get(
-      `${this.basicUrl}/${this.currentVersion}/${endpoint}/${longitude},${latitude}.json?${query}`,
-    )).data as GeocodeType
+      `${this.basicUrl}/${this.currentVersion}/${endpoint}/${longitude},${latitude}.json`, {
+      params: {
+        ...reverseOption,
+        endpoint,
+        access_token: getAccessToken() || '',
+      }
+    })).data as GeocodeType
   }
 
   static async forward({ endpoint = 'mapbox.places', searchText }: ForwardGeocoding) {
     checkAccessToken()
     return (await axios.get(
-      `${this.basicUrl}/${this.currentVersion}/${endpoint}/${searchText}.json?access_token=${getAccessToken()}`,
-    )).data as GeocodeType
+      `${this.basicUrl}/${this.currentVersion}/${endpoint}/${searchText}.json`,
+      {
+        params: {
+          endpoint,
+          searchText,
+          access_token: getAccessToken() || '',
+        }
+      })).data as GeocodeType
   }
 }
 
